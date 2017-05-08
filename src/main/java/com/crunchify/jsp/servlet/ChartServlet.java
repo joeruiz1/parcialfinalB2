ab4b2c1a272a95271b1fe0c8e4957acd5d076399
fernando stiven <fernando stiven@fernandostiven>
2017-05-08 11:56:53 -0500

    holi2.0

--- src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
@@ -18,40 +18,49 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.general.DefaultPieDataset;
+import edu.co.sergio.mundo.dao.DepartamentoDAO;
 
 public class ChartServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    DepartamentoDAO dep = new DepartamentoDAO();
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        	response.setContentType("image/png");
-		OutputStream outputStream = response.getOutputStream();
-		JFreeChart chart = getChart();
-		int width = 500;
-		int height = 350;
-		ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("image/png");
+        OutputStream outputStream = response.getOutputStream();
+        JFreeChart chart = getChart();
+        int width = 500;
+        int height = 350;
+        ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
 
-	}
+    }
 
-	public JFreeChart getChart() {
-		
-                DefaultPieDataset dataset = new DefaultPieDataset();
-	        //Crear la capa de servicios que se enlace con el DAO
-                dataset.setValue("Ford", 23.3);
-		dataset.setValue("Chevy", 32.4);
-		dataset.setValue("Yugo", 44.2);
+    public JFreeChart getChart() {
 
-		boolean legend = true;
-		boolean tooltips = false;
-		boolean urls = false;
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        //Crear la capa de servicios que se enlace con el DAO
+        dep.recursos();
+        for (int i = 0; i < dep.recursos().size(); i++) {
 
-		JFreeChart chart = ChartFactory.createPieChart("Cars", dataset, legend, tooltips, urls);
+            dep.recursos().get(i).getName_proy();
+            dep.recursos().get(i).getTotal();
+        }
 
-		chart.setBorderPaint(Color.GREEN);
-		chart.setBorderStroke(new BasicStroke(5.0f));
-		chart.setBorderVisible(true);
+//        dataset.setValue("Ford", 23.3);
+//        dataset.setValue("Chevy", 32.4);
+//        dataset.setValue("Yugo", 44.2);
 
-		return chart;
-	}
+        boolean legend = true;
+        boolean tooltips = false;
+        boolean urls = false;
+
+        JFreeChart chart = ChartFactory.createPieChart("Cars", dataset, legend, tooltips, urls);
+
+        chart.setBorderPaint(Color.GREEN);
+        chart.setBorderStroke(new BasicStroke(5.0f));
+        chart.setBorderVisible(true);
+
+        return chart;
+    }
 
 }
